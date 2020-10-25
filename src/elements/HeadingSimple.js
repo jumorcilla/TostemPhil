16656f117bab7ae621661063f0f61e150d94c45b
jumorcilla <juliusrei.morcilla@egis.ph>
2020-10-25 18:50:45 +0800

    Updates

--- src/elements/HeadingSimple.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const HeadingSimple = ({ title }) => (
-  <div className="col-sm-8 section-heading">
-    <h2>{title}</h2>
-  </div>
-);
-
-export default HeadingSimple;
